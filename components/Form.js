bec0cc1a43b1243973ec203be595df2b04ee4d9b
Kizmelvin <chidiebere.johnbosco@gmail.com>
2022-08-08 10:42:24 +0100

    initial commit

---
+++ components/Form.js
@@ -0,0 +1,262 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+function Form({
+  userDetails,
+  setUserDetails,
+  handleSubmit,
+  enableInputs,
+  setEnableInputs,
+}) {
+  const {
+    firstname,
+    lastname,
+    email,
+    mobile,
+    website,
+    course,
+    role,
+    company,
+    position,
+    school,
+    country,
+    state,
+    interest_1,
+    interest_2,
+    interest_3,
+  } = userDetails;
+
+  const handleChange = (e) => {
+    setUserDetails({ ...userDetails, [e.target.name]: e.target.value });
+    setEnableInputs(true);
+  };
+
+  return (
+    <div className="container">
+      <div className="mt-3">
+        <h1>Resume Generator </h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h4>Please Input Your Details</h4>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="firstname">Firstname</label>
+            <input
+              type="text"
+              className="form-control"
+              id="firstname"
+              placeholder="Firstname"
+              name="firstname"
+              required
+              //   disabled={enableInputs}
+              value={firstname}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="lastname">Lastname</label>
+            <input
+              type="text"
+              className="form-control"
+              id="lastname"
+              placeholder="Lastname"
+              name="lastname"
+              required
+              //   disabled={enableInputs}
+              value={lastname}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="role">Role</label>
+            <input
+              type="text"
+              className="form-control"
+              id="role"
+              placeholder="Your Role"
+              name="role"
+              required
+              //   disabled={enableInputs}
+              value={role}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              className="form-control"
+              id="country"
+              name="country"
+              required
+              //   disabled={enableInputs}
+              value={country}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6 mb-2">
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              id="state"
+              name="state"
+              className="form-control"
+              required
+              //   disabled={enableInputs}
+              value={state}
+              onChange={handleChange}
+            />
+          </div>
+          <h5>Contact</h5>
+          <div className="form-group col-md-6">
+            <label htmlFor="inputEmail">Email</label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              placeholder="Email"
+              name="email"
+              required
+              //   disabled={enableInputs}
+              value={email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="mobile">Mobile</label>
+            <input
+              type="text"
+              className="form-control"
+              id="mobile"
+              placeholder="Phone number"
+              name="mobile"
+              required
+              //   disabled={enableInputs}
+              value={mobile}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6 mb-2">
+            <label htmlFor="phone">Website</label>
+            <input
+              type="text"
+              className="form-control"
+              id="website"
+              placeholder="website"
+              name="website"
+              required
+              //   disabled={enableInputs}
+              value={website}
+              onChange={handleChange}
+            />
+          </div>
+          <h5>Education</h5>
+          <div className="form-group col-md-6">
+            <label htmlFor="school">School</label>
+            <input
+              type="text"
+              className="form-control"
+              id="school"
+              name="school"
+              //   disabled={enableInputs}
+              value={school}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group col-md-6 mb-2">
+            <label htmlFor="course">Course of Study</label>
+            <input
+              type="text"
+              className="form-control "
+              id="course"
+              name="course"
+              required
+              //   disabled={enableInputs}
+              value={course}
+              onChange={handleChange}
+            />
+          </div>
+          <h5>Experience</h5>
+          <div className="form-group col-md-6">
+            <label htmlFor="company">Company</label>
+            <input
+              type="text"
+              className="form-control"
+              id="company"
+              placeholder="company"
+              name="company"
+              required
+              //   disabled={enableInputs}
+              value={company}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6 mb-2">
+            <label htmlFor="position">Position</label>
+            <input
+              type="text"
+              className="form-control"
+              id="position"
+              placeholder="position"
+              name="position"
+              required
+              //   disabled={enableInputs}
+              value={position}
+              onChange={handleChange}
+            />
+          </div>
+          {/* Interests form */}
+          <h5>Interests</h5>
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              id="interest_1"
+              placeholder="interest"
+              name="interest_1"
+              required
+              //   disabled={enableInputs}
+              value={interest_1}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6 mt-2">
+            <input
+              type="text"
+              className="form-control"
+              id="interest_2"
+              placeholder="interest"
+              name="interest_2"
+              required
+              //   disabled={enableInputs}
+              value={interest_2}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group col-md-6 mt-2">
+            <input
+              type="text"
+              className="form-control"
+              id="interest_3"
+              placeholder="interest"
+              name="interest_3"
+              required
+              //   disabled={enableInputs}
+              value={interest_3}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="btn btn-primary mt-3"
+        >
+          Generate Resume
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
---
+++ components/Template.js
@@ -0,0 +1,163 @@
+import { Cloudinary } from "@cloudinary/url-gen";
+import { source } from "@cloudinary/url-gen/actions/overlay";
+import { text } from "@cloudinary/url-gen/qualifiers/source";
+import { TextStyle } from "@cloudinary/url-gen/qualifiers/textStyle";
+import { Position } from "@cloudinary/url-gen/qualifiers";
+import { compass } from "@cloudinary/url-gen/qualifiers/gravity";
+
+function Home({ userDetails, setUserDetails, setShowPDF, initialState }) {
+  const {
+    firstname,
+    lastname,
+    email,
+    mobile,
+    website,
+    course,
+    role,
+    company,
+    position,
+    school,
+    country,
+    state,
+    interest_1,
+    interest_2,
+    interest_3,
+  } = userDetails;
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "Kizmelvin",
+    },
+  });
+
+  const template = cld
+    .image("local-uploads/zq5mfbimt1vxjhrdbn1l.pdf")
+    .addFlag("rasterize");
+
+  template.overlay(
+    source(
+      text(`${firstname} ${lastname}`, new TextStyle("Nunito", 60)).textColor(
+        "black"
+      )
+    ).position(
+      new Position().gravity(compass("north")).offsetY(200).offsetX(-400)
+    )
+  );
+  //Education Overlay
+  template
+    .overlay(
+      source(text(`${school}`, new TextStyle("Times", 30))).position(
+        new Position().gravity(compass("west")).offsetY(50).offsetX(100)
+      )
+    )
+    .overlay(
+      source(
+        text(`${course}`, new TextStyle("Times", 30)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("west")).offsetY(120).offsetX(100)
+      )
+    );
+  // Contacts Overlay
+  template
+    .overlay(
+      source(
+        text(`${mobile}`, new TextStyle("Times", 30)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("west")).offsetY(410).offsetX(100)
+      )
+    )
+    .overlay(
+      source(
+        text(`${email}`, new TextStyle("Times", 30)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("west")).offsetY(510).offsetX(100)
+      )
+    )
+    .overlay(
+      source(
+        text(`${website}`, new TextStyle("Times", 30)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("west")).offsetY(610).offsetX(100)
+      )
+    );
+  // Profile Overlays
+  template
+    .overlay(
+      source(
+        text(
+          `I'm ${firstname}  ${lastname},`,
+          new TextStyle("Times", 40)
+        ).textColor("gray")
+      ).position(
+        new Position().gravity(compass("north")).offsetY(300).offsetX(200)
+      )
+    )
+    .overlay(
+      source(
+        text(
+          `a ${role} from ${state}, ${country}.`,
+          new TextStyle("Times", 30)
+        ).textColor("gray")
+      ).position(
+        new Position().gravity(compass("north")).offsetY(350).offsetX(200)
+      )
+    );
+  // Experience Overlay
+  template
+    .overlay(
+      source(
+        text(`${company}`, new TextStyle("Times", 45)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("north")).offsetY(700).offsetX(200)
+      )
+    )
+    .overlay(
+      source(
+        text(`${position}`, new TextStyle("Times", 30)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("north")).offsetY(750).offsetX(200)
+      )
+    );
+  // Interests Overlay
+  template
+    .overlay(
+      source(
+        text(`${interest_1}`, new TextStyle("Times", 45)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("south")).offsetY(540).offsetX(210)
+      )
+    )
+    .overlay(
+      source(
+        text(`${interest_2}`, new TextStyle("Times", 45)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("south")).offsetY(450).offsetX(180)
+      )
+    )
+    .overlay(
+      source(
+        text(`${interest_3}`, new TextStyle("Times", 45)).textColor("gray")
+      ).position(
+        new Position().gravity(compass("south")).offsetY(360).offsetX(200)
+      )
+    );
+  const pdfTemplate = template.toURL();
+  return (
+    <div>
+      <iframe src={pdfTemplate} width="500" height="778"></iframe>
+      <div className="mt-3">
+        <button
+          onClick={() => {
+            setUserDetails(initialState);
+            setShowPDF(false);
+          }}
+          className="btn btn-primary"
+        >
+          Generate New Resume
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
